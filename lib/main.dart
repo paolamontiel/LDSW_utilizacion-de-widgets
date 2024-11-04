007fc64fa0eb9e10b02ada3a2f01f6a440bc5aac
Paola Montiel <paolamontiel.dg@gmail.com>
2024-11-03 21:43:53 -0600

    Act 3.5 Home Screen

--- lib/main.dart
+++ lib/main.dart
@@ -8,101 +8,91 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Utilización de widgets',
+      title: 'Catalogo de Películas',
       theme: ThemeData(
         useMaterial3: true,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: const Color.fromARGB(255, 34, 255, 218)),
       ),
-      home: MyHomePage(),
+      home: HomeScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('Actividad 3.4 Utilización de widgets',
-          style: TextStyle( fontSize: 24, fontWeight: FontWeight.bold, ),
-        ),
-      ),
-      body: Center(
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/background-app.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Column(
             children: [
-
-              Text('Widget Text:', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold), ),
-              Text('"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."', style: TextStyle(fontSize: 16),),
-              SizedBox(height: 20),
-
-              Text('Widget Row:', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold), ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(Icons.star, color: Colors.blue, size: 30),
-                  SizedBox(width: 10),
-                  Icon(Icons.star, color: Colors.green, size: 30),
-                  SizedBox(width: 10),
-                  Icon(Icons.star, color: Colors.red, size: 30),
-                ],
-              ),
-              SizedBox(height: 20),
-
-              Text('Widget Column:', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold), ),
-              Column(
-                children: [
-                  Text('Elemento 1', style: TextStyle(fontSize: 16), ),
-                  Text('Elemento 2', style: TextStyle(fontSize: 16), ),
-                  Text('Elemento 3', style: TextStyle(fontSize: 16), ),
-                ],
-              ),
-              SizedBox(height: 20),
-
-              Text('Widget Container:', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),),
               Container(
-                width: 100,
-                height: 100,
-                color: Colors.purple,
+                padding: EdgeInsets.symmetric(vertical: 20),
+                margin: EdgeInsets.only(top: 50),
                 child: Center(
-                  child: Text('Container', style: TextStyle(color: Colors.white, fontSize: 16), ),
-                ),
-              ),
-              SizedBox(height: 20),
-
-
-              Text('Widget Stack:',
-                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-              ),
-              Stack(
-                alignment: Alignment.center,
-                children: [
-                  Container(
-                    width: 150,
-                    height: 150,
-                    color: Colors.orange,
-                  ),
-                  Container(
-                    width: 100,
-                    height: 100,
-                    color: Colors.blue,
-                  ),
-                  Container(
-                    width: 50,
-                    height: 50,
-                    color: Colors.white,
-                    child: Center(
-                      child: Text('Stack', style: TextStyle(color: Colors.black, fontSize: 16), ),
+                  child: Text(
+                    'Catálogo de Películas',
+                    style: TextStyle(
+                      fontSize: 42,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                      shadows: [
+                        Shadow(
+                          blurRadius: 10.0,
+                          color: Colors.black54,
+                          offset: Offset(2.0, 2.0),
+                        ),
+                      ],
                     ),
                   ),
-                ],
+                ),
               ),
-              SizedBox(height: 20),
+              Expanded(
+                child: Center(
+                  child: Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.movie_filter,
+                          color: Colors.white,
+                          size: 80,
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          'Hello World\nDisfruta de una amplia colección de películas aclamadas por la crítica',
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            fontSize: 24,
+                            color: Colors.white,
+                            fontWeight: FontWeight.w300,
+                            shadows: [
+                              Shadow(
+                                blurRadius: 10.0,
+                                color: Colors.black54,
+                                offset: Offset(2.0, 2.0),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
-        ),
+        ],
       ),
     );
   }
